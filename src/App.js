fcbf5c089c1f142b49c8bb3eb9a76d28b7226e17
Hayk Bazoyan <bazoyan13@gmail.com>
2021-08-10 10:48:49 +0400

    feat: add tailwind css

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-import './App.css';
+// import "./App.css";
 
 function App() {
   return (
     <div className="App">
-     
+      <p className="text-red-500 text-5xl">hello</p>
+      <p className="text-5xl">hello</p>
     </div>
   );
 }

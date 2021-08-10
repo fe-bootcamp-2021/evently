ea0cf028d6286b7969ab30567b9ee974e4f28655
Arevik Hayrapetyan <arevhayrapetyan88@gmail.com>
2021-08-10 14:29:39 +0400

    feat(Navbar): Added Navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
-import './App.css';
+import React from "react";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import Home from "./components/Home/Home";
+import Account from "./components/Account/Account";
+import Navbar from "./components/Navbar/Navbar";
 
 function App() {
   return (
-    <div className="App">
-     aaaaaaa
-    </div>
+    <BrowserRouter>
+      <Navbar />
+      <div className="App">
+        <Switch>
+          <Route exact path="/home" component={Home} />
+          <Route path="/account" component={Account} />
+        </Switch>
+      </div>
+    </BrowserRouter>
   );
 }
 

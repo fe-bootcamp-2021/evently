5b1e6000fbe6d748c59332fabfea256614e20662
Tigran Khanaghyan <arm.mail1991@gmail.com>
2021-08-17 00:01:48 +0400

    feat: add availability component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from "react";
-import {MyCalendar} from './components/Availability/ListView'
-import Login from "./components/Login/Login";
+import Demo from './components/Availability/ListView'
+// import Login from "./components/Login/Login";
 
 function App() {
   return (
     <div className="App">
-      <Login />
+      <Demo />
     </div>
   );
 }
--- src/components/Availability/ListView.jsx
+++ src/components/Availability/ListView.jsx
@@ -1,31 +1,91 @@
-import { Calendar, dateFnsLocalizer } from 'react-big-calendar'
-import format from 'date-fns/format'
-import parse from 'date-fns/parse'
-import startOfWeek from 'date-fns/startOfWeek'
-import getDay from 'date-fns/getDay'
-import enUS from 'date-fns/locale/en-US'
+import * as React from "react";
+import Paper from "@material-ui/core/Paper";
+import { ViewState, EditingState } from "@devexpress/dx-react-scheduler";
+import {
+  Toolbar,
+  TodayButton,
+  DateNavigator,
+  Scheduler,
+  Resources,
+  MonthView,
+  Appointments,
+  AppointmentTooltip,
+  AppointmentForm,
+  EditRecurrenceMenu,
+  DragDropProvider,
+} from "@devexpress/dx-react-scheduler-material-ui";
+import { owners } from "./data/tasks";
+import { appointments, resourcesData } from "./data/resourses";
 
+export default class Demo extends React.PureComponent {
+  constructor(props) {
+    super(props);
+    this.state = {
+      date: new Date(),
+      data: appointments,
+      resources: [
+        {
+          fieldName: "roomId",
+          title: "Room",
+          instances: resourcesData,
+        },
+        {
+          fieldName: "members",
+          title: "Members",
+          instances: owners,
+          allowMultiple: true,
+        },
+      ],
+    };
 
-const locales = {
-  'en-US': enUS
+    this.commitChanges = this.commitChanges.bind(this);
+  }
+
+  commitChanges({ added, changed, deleted }) {
+    this.setState((state) => {
+      let { data } = state;
+      if (added) {
+        const startingAddedId =
+          data.length > 0 ? data[data.length - 1].id + 1 : 0;
+        data = [...data, { id: startingAddedId, ...added }];
+      }
+      if (changed) {
+        data = data.map((appointment) =>
+          changed[appointment.id]
+            ? { ...appointment, ...changed[appointment.id] }
+            : appointment
+        );
+      }
+      if (deleted !== undefined) {
+        data = data.filter((appointment) => appointment.id !== deleted);
+      }
+      return { data };
+    });
+  }
+
+  render() {
+    const { data, resources, date } = this.state;
+
+    return (
+      <Paper>
+        <Scheduler data={data}>
+          <ViewState defaultCurrentDate={date} />
+          <MonthView />
+          <Toolbar />
+          <DateNavigator />
+          <TodayButton />
+          <EditingState onCommitChanges={this.commitChanges} />
+          <EditRecurrenceMenu />
+
+          <MonthView />
+          <Appointments />
+          <AppointmentTooltip showOpenButton />
+          <AppointmentForm />
+
+          <Resources data={resources} mainResourceName="roomId" />
+          <DragDropProvider />
+        </Scheduler>
+      </Paper>
+    );
+  }
 }
-
-const localizer = dateFnsLocalizer({
-  format,
-  parse,
-  startOfWeek,
-  getDay,
-  locales,
-})
-
-export const MyCalendar = props => (
-  <div>
-    <Calendar
-      localizer={localizer}
-    //   events={myEventsList}
-      startAccessor="start"
-      endAccessor="end"
-      style={{ height: 500 }}
-    />
-  </div>
-)

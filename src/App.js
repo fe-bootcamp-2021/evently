bfcddccdfe8e443473715038b7b46054533f92bc
ani-kesh <aniqeshishyan1992@gmail.com>
2021-08-18 17:41:20 +0400

    feat: changed button component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Login from "./components/LoginSign/LoginSign";
+import OneOnOne from "./components/OneOnOne/OneOnOne";
 
 export default function App() {
   return (
     <div className="App">
       <Login />
+      <OneOnOne/>
     </div>
   );
 }

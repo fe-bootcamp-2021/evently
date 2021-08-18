1ccbca35cab045e71f1ed9ee550525d29ace394d
Hayk Bazoyan <bazoyan13@gmail.com>
2021-08-18 18:28:21 +0400

    feat: add Button component in Dropdown.jsx

--- src/components/Dropdown/Dropdown.jsx
+++ src/components/Dropdown/Dropdown.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { BrowserRouter as Router, Link } from "react-router-dom";
 import classNames from "classnames/bind";
+import Button from "../Button/Button"
 
 export default function Dropdown({buttonName}) {
   const [show, setShow] = useState(false); 
@@ -20,14 +21,12 @@ export default function Dropdown({buttonName}) {
   return (
     <div>
       <div className="flex flex-col justify-end"> 
-        <button onClick={showFn} className="mt-4 border-0 block bg-blue-600 bg-blue-600 text-gray-200 rounded-lg px-11 text-sm py-3 overflow-hidden focus:outline-none focus:border-white">
-          {buttonName}
-        </button>
+        <Button name={buttonName} onClick={showFn} style="mt-4 border-0 block bg-blue-800 hover:bg-blue-600 text-white font-bold rounded-full px-11 py-3 overflow-hidden focus:outline-none focus:border-white" />
         
         <div className={dropDownMenu}> 
           <Router>
-            <Link to="#" className="block text-sl px-4 py-2 rounded-t-lg text-gray-800 hover:bg-purple-200 hover:text-white">One-on-One</Link> 
-            <Link to="#" className="block text-sl px-4 py-2 rounded-b-lg text-gray-800 hover:bg-purple-200 hover:text-white">Group Meeting</Link> 
+            <Link to="#" className="block text-sl px-4 py-2 rounded-t-lg text-gray-800 hover:text-purple-700 hover:underline">One-on-One</Link> 
+            <Link to="#" className="block text-sl px-4 py-2 rounded-b-lg text-gray-800 hover:text-purple-700 hover:underline">Group Meeting</Link> 
           </Router>
         </div>
       </div>

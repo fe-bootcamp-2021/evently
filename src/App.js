e3a1123bce1917fa18589e85dc640e8909385bde
ani-kesh <aniqeshishyan1992@gmail.com>
2021-08-24 15:46:35 +0400

    merge auth with dev

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 import React from "react";
 import Main from "../src/components/LoginSign/LoginSign";
-import Login from "../src/components/LoginSign/LoginSign";
 import { AuthProvider } from "./contexts/AuthContext";
 
 export default function App() {
   return (
     <AuthProvider>
     <div className="App">
-      <Login />
+      <Main />
     </div>
   </AuthProvider>
 
--- src/services/base.js
+++ src/services/base.js
@@ -1,11 +1,8 @@
-<<<<<<< HEAD
-import firebase from "firebase/app"
-import "firebase/auth"
-=======
+
 import firebase from "firebase/app";
 import "firebase/auth";
 import "firebase/database";
->>>>>>> verification
+
 
 const app = firebase.initializeApp({
   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
@@ -15,17 +12,11 @@ const app = firebase.initializeApp({
   storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
   messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
   appId: process.env.REACT_APP_FIREBASE_APP_ID,
-<<<<<<< HEAD
-  measurementId: process.env.REACT_APP_MEASUREMENT_ID
-})
 
-export const auth = app.auth()
-export default app
-=======
   measurementId: process.env.REACT_APP_MEASUREMENT_ID,
 });
 
 export const auth = app.auth();
 export const db = firebase.database();
 export default app;
->>>>>>> verification
+

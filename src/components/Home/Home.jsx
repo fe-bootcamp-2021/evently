09e42b366354a60e858db5d4a3c68a1aff7a97b4
ani-kesh <aniqeshishyan1992@gmail.com>
2021-08-24 15:00:56 +0400

    refactoing: home component

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,57 +1,40 @@
-import React, { useState } from "react";
-import { BrowserRouter as Router, Link } from "react-router-dom";
+import React from "react";
 import Card from "../Card/Card";
 import Dropdown from "../Dropdown/Dropdown";
-import Logo from "../Logo/Logo";
-import Button from "../Button/Button"
+import Button from "../Button/Button";
+import Navbar from "../Navbar/Navbar";
 
 export default function Home() {
-    return (
-        <div>
-            <div className="max-w-lm h-48 inline-block flex flex-col mx-auto bg-white shadow-md items-center ">
-                <div className="w-9/12">
-                    <div className="mt-5 flex justify-between">
-                        <Logo />
-                        <div className="flex justify-between items-center">
-                            <Router>
-                                <Link to="#" className="mr-8 text-blue-900 hover:text-purple-800 hover:font-extrabold">Home</Link> 
-                                <Link to="#" className="mr-8 text-blue-900 hover:text-purple-800 hover:font-extrabold">Help</Link>
-                                <Link to="#" className="text-blue-900 hover:text-purple-800 hover:font-extrabold">Log Out</Link> 
-                            </Router>
-                        </div>
-                    </div>
-                    <div className="flex justify-between items-center  mt-8 mb-4">
-                        <div className="flex flex-col justify-between items-left mb-4">
-                            <h1 className="text-2xl text-blue-900 mb-4">My Meetings</h1>
-                            <div className="flex mb-6">
-                                <Button name="Event Types" style="text-sm mr-4 font-semibold text-blue-700 hover:text-purple-700" />
-                                <Button name="Scheduled Events" style="text-sm mr-4 font-semibold text-blue-700 hover:text-purple-700" />
-                            </div>
-                        </div>
-                    
-                        <div className="h-36">
-                            <Dropdown buttonName="Create +" />
-                        </div>
-
-                    </div>
-                </div>
+  return (
+    <div>
+      <div className="max-w-lm h-48 inline-block flex flex-col mx-auto bg-white shadow-md items-center ">
+        <div className="w-9/12">
+          <div className="mt-5 flex justify-between">
+            <Navbar />
+          </div>
+          <div className="flex justify-between items-center  mt-8 mb-4">
+            <div className="flex flex-col justify-between items-left mb-4">
+              <h1 className="text-2xl text-blue-900 mb-4">My Meetings</h1>
+              <div className="flex mb-6">
+                <Button name="Event Types" variant="link" />
+                <Button name="Scheduled Events" variant="link" />
+              </div>
             </div>
-            
-            <div className="w-9/12 mt-6 mx-auto items-center">
-                <div className="flex  flex-col  md:flex-row justify-between  flex-wrap gap-3 mt-10">
-                    <Card />
-                    <Card />
-                    <Card />
-                </div>
+
+            <div className="h-36">
+              <Dropdown buttonName="Create +" />
             </div>
-            
-
-
+          </div>
         </div>
-        
+      </div>
 
-
-
-
-    )
+      <div className="w-9/12 mt-6 mx-auto items-center">
+        <div className="flex  flex-col  md:flex-row justify-between  flex-wrap gap-3 mt-10">
+          <Card />
+          <Card />
+          <Card />
+        </div>
+      </div>
+    </div>
+  );
 }
--- src/components/UnAuthenticatedApp/UnAuthenticatedApp.jsx
+++ src/components/UnAuthenticatedApp/UnAuthenticatedApp.jsx
@@ -1,7 +1,5 @@
-import React, { useState } from "react";
+import React from "react";
 import { BrowserRouter as Router, Link } from "react-router-dom";
-import Card from "../Card/Card";
-import Dropdown from "../Dropdown/Dropdown";
 import Logo from "../Logo/Logo";
 import Button from "../Button/Button"
 import Input from "../Input/Input"
@@ -32,7 +30,7 @@ export default function Main() {
                     <h2 className="mt-8 text-blue-900">We will help you schedule meetings <br /> on each day.</h2>
                     <div className="flex mt-4">
                         <Input type="email" autocomplete="on" className="w-72 rounded-l-full p-4 border mr-0 text-blue-900 border-blue-900 bg-white outline-none" placeholder="Enter Your Email" />
-                        <Button name="Sign In" style="bg-blue-800 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded-r-full" />
+                        <Button name="Sign In" className="bg-blue-800 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded-r-full" variant="custom"/>
                     </div>
                 </div>
 
@@ -44,24 +42,24 @@ export default function Main() {
                     <div className="rounded-full absolute bg-purple-900 ring-4 ring-purple-200 w-7 h-7 top-0 right-0"></div>
                     <div className="rounded-full absolute bg-purple-900 ring-4 ring-purple-200 w-7 h-7 inset-x-6 top-20"></div>
                         <div className="rounded-full absolute w-14 h-14 -inset-4">
-                            <img src={Image} className="-inset-4" />
+                            <img src={Image} className="-inset-4" alt="" />
                         </div>
                         <div className="rounded-full absolute w-14 h-14 top-5 right-12">
-                            <img src={Image2} className="top-5 right-12" />
+                            <img src={Image2} className="top-5 right-12" alt="" />
                         </div>
                         <div className="rounded-full absolute w-14 h-14 inset-x-10 bottom-7">
-                            <img src={Image} className="inset-x-10 bottom-7" />
+                            <img src={Image} className="inset-x-10 bottom-7"  alt=""/>
                         </div>
                     </div>
                     <div className="rounded-full absolute border border-purple-900 w-72 h-72">
                         <div className="rounded-full absolute bg-purple-900 ring-4 ring-purple-200 w-7 h-7 bottom-10 left-4"></div>
                         <div className="rounded-full absolute bottom-5 right-2">
-                            <img src={Image2} className="w-14 h-14" />
+                            <img src={Image2} className="w-14 h-14"  alt=""/>
                         </div>
                     </div>
                     <div className="rounded-full absolute border border-purple-900"></div>
                     <div className="rounded-full absolute w-28 h-28">
-                        <img src={Image} className="w-28 h-28" />
+                        <img src={Image} className="w-28 h-28"  alt=""/>
                     </div>  
                 </div>
             </div>

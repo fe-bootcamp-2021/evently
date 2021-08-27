f37e7e63823100cb2c366822fb53c27754888ce4
KarineAsatryan <asatryankarine71@gmail.com>
2021-08-27 12:58:18 +0400

    add team members

--- src/pages/PageAboutUS/PageAboutUS.jsx
+++ src/pages/PageAboutUS/PageAboutUS.jsx
@@ -11,82 +11,80 @@ export default function PageAboutUS() {
       <div className="max-w-lm inline-block flex flex-col mx-auto bg-white shadow-md">
         <Logo />
       </div>
-      <div className="block mt-1 text-lg leading-tight font-bold text-center">
-        About us
-        <div className="placeholder-blue-500 italic text-2xl text-blue-400 p-50 text-lg extralight font-mono">
-          "COME MEETING" will help you better plan your day. We are glad to help
-          you.
+      <div>
+        <div className="text-center p-10">
+          <h2 className="text-5xl text-blue-800 text-center mt-15">
+            Plan Your Day Ahead
+          </h2>
+          <h2 className="mt-8 text-blue-700">
+            We will help you schedule meetings on each day.
+          </h2>
         </div>
-        <h4 className="capitalize not-italic">
+      </div>
+      <div
+        className="text-3xl text-gray-500 text-center">
+        "COME MEETING" will help you better plan your day. We are glad to help
+        you.
+        <h4 className="not-italic">
           You can arrange group or individual meetings with the help of "COME
           MEETING".
         </h4>
-        <h2 className="uppercase text-blue-400 italic">Meet the leadership</h2>
         <h2>Our team believes anything is possible.</h2>
-      </div>
-      {/* <div className="flex justify-center p-15">
+        <br/>
+        <h2 className="uppercase text-blue-900 italic  mt-4 text-center text-6xl">Meet the leadership</h2>
+      </div>  
+      <div className="flex justify-center p-15 mt-4">
         <div className="flex space-x-7">
           <div className="justify-center space-y-3">
             <Avatar className="h-3 w-3 p-20 border-solid border-gray-900 shadow-lg">
               TK
             </Avatar>
-            <div className="justify-center space-y-1">
+            <div className="text-center space-y-1 ">
               <h3> Tigran Khanaghyan</h3>
               <h3> Team brain</h3>
             </div>
           </div>
-        </div>
-        <div className="flex justify-center p-15">
-        <div className="flex space-x-7">
-          <div className="justify-center space-y-3">
+          <div className="text-center space-y-3">
             <Avatar className="h-3 w-3 p-20 border-solid border-gray-900 shadow-lg">
               AK
             </Avatar>
-            <div className="justify-center space-y-1">
+            <div className="text-center space-y-1">
               <h3> Ani Keshishyan </h3>
               <h3> Team omniscient </h3>
             </div>
           </div>
-        </div>
-        <div className="flex justify-center p-15">
-        <div className="flex space-x-7">
-          <div className="justify-center space-y-3">
+          <div className="text-center space-y-3">
             <Avatar className="h-3 w-3 p-20 border-solid border-gray-900 shadow-lg">
               HB
             </Avatar>
-            <div className="justify-center space-y-1">
+            <div className="text-center space-y-1">
               <h3> Haik Bazoyan </h3>
               <h3> Team designer</h3>
             </div>
           </div>
-        </div>
-        <div className="flex justify-center p-15">
-        <div className="flex space-x-7">
-          <div className="justify-center space-y-3">
+          <div className="text-center space-y-3">
             <Avatar className="h-3 w-3 p-20 border-solid border-gray-900 shadow-lg">
               KA
             </Avatar>
-            <div className="justify-center space-y-1">
+            <div className="text-center space-y-1">
               <h3> Karine Asatryan</h3>
               <h3> Our beginner</h3>
             </div>
           </div>
-        </div>
-        <div className="flex justify-center p-15">
-         <div className="flex space-x-7">
-          <div className="justify-center space-y-3">
+          <div className="text-center space-y-3">
             <Avatar className="h-3 w-3 p-20 border-solid border-gray-900 shadow-lg">
               AH
             </Avatar>
-            <div className="justify-center space-y-1">
+            <div className="text-center space-y-1">
               <h3> Arevik Hayrapetyan </h3>
               <h3> Our sun </h3>
             </div>
           </div>
-          </>div */}
-        {/* </div>
         </div>
-      <AboutUsFooter /> */}
+      </div> 
+      <AboutUsFooter />
     </div>
   );
-}
+};
+
+

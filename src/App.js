2c6952c1118b224a0247f4ce2a212bf9d99c462b
Tigran Khanaghyan <arm.mail1991@gmail.com>
2021-08-29 19:30:18 +0400

    feat: add members

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React from "react";
 import Main from "../src/components/LoginSign/LoginSign";
 import { AuthProvider } from "./contexts/AuthContext";
-
+import Home from "./components/Home/Home";
+import GroupEventFirstPage from "./components/GroupEventPage/GroupEventFirstPage";
+import EventScheduler from "./components/Availability/ListView";
 export default function App() {
   return (
     <AuthProvider>
     <div className="App">
-      <Main />
+      {/* <Main/>
+      <GroupEventFirstPage /> */}
+      <EventScheduler/>
     </div>
   </AuthProvider>
 
--- src/components/Availability/ListView.jsx
+++ src/components/Availability/ListView.jsx
@@ -19,7 +19,7 @@ import { appointments, resourcesData } from "./data/resourses";
 
 
 
-export default class Evently extends React.PureComponent {
+export default class EventScheduler extends React.PureComponent {
   constructor(props) {
     super(props);
     this.state = {
@@ -61,6 +61,7 @@ export default class Evently extends React.PureComponent {
       if (deleted !== undefined) {
         data = data.filter((appointment) => appointment.id !== deleted);
       }
+      console.log({data})
       return { data };
     });
   }

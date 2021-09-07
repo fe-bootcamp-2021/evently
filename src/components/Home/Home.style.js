346686424459d1d52115c2c9d219291eef7e73ff
ani-kesh <aniqeshishyan1992@gmail.com>
2021-09-07 12:38:38 +0400

    refactor: home page

---
+++ src/components/Home/Home.style.js
@@ -0,0 +1,14 @@
+import classNames from "classnames/bind";
+
+export const headContainer = classNames([
+  "max-w-lm h-48 inline-block flex flex-col mx-auto bg-white shadow-md items-center ",
+]);
+export const myMeeting = classNames(["text-2xl text-blue-900 mb-4"]);
+export const leftSide = classNames([
+  "flex flex-col justify-between items-left mb-4",
+]);
+export const subHeader = classNames(["flex justify-between items-center"]);
+export const content = classNames(["w-9/12 mt-6 mx-auto items-center"]);
+export const cardContainer = classNames([
+  "grid 2xl:grid-cols-3 sm:grid-cols-2 gap-4 mt-10",
+]);

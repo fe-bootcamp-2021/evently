11b1974ad2935ce99a0b1b1ef517443f51641d61
Arevik Hayrapetyan <arevhayrapetyan88@gmail.com>
2021-09-08 14:00:05 +0400

    feat(validation.js): Added time validation

--- src/App.js
+++ src/App.js
@@ -1,19 +1,4 @@
 import React from "react";
-<<<<<<< HEAD
-import Login from "./components/LoginSign/LoginSign";
-import OneOnOne from "./components/OneOnOne/OneOnOne";
-import GroupEventFirstPage from "./components/GroupEventPage/GroupEventFirstPage";
-import Evently from "./components/Availability/ListView";
-
-export default function App() {
-  return (
-    <div className="App">
-      <Login />
-      <OneOnOne />
-      <GroupEventFirstPage />
-      <Evently />
-    </div>
-=======
 import { useAuth } from "./contexts/AuthContext";
 import AuthenticatedApp from "./components/AuthenticatedApp/AuthenticatedApp";
 import UnAuthenticatedPage from "./pages/UnAuthenticatedPage/UnAuthenticated.page";
@@ -23,6 +8,5 @@ export default function App() {
 
   return (
     <>{isAuthenticated ? <AuthenticatedApp /> : <UnAuthenticatedPage />}</>
->>>>>>> 618abd85ab6590427ce18cc3d6a64a92a0ce5909
   );
 }

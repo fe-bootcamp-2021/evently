da7dd19c0bb46cc4d8549b47b8f61bb7aa088aa8
Tigran Khanaghyan <arm.mail1991@gmail.com>
2021-09-14 06:14:11 +0400

    fix Loading error

--- src/components/Loading/Loading.style.js
+++
@@ -1,8 +0,0 @@
-import classNames from "classnames/bind";
-
-export const loadingContainer = classNames([
-  "w-full h-screen flex justify-center items-center",
-]);
-export const loading = classNames([
-  "animate-spin rounded-full h-32 w-32 border-t-2 border-b-2 border-purple-700",
-]);
